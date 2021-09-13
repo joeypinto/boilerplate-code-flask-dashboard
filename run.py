9c7514482391558d08a5f568bdc0ca54919a9f21
ksh7 <kundansingh.connect@gmail.com>
2021-09-13 20:37:07 +0530

    folder structure improvements

--- run.py
+++ run.py
@@ -7,8 +7,8 @@ from flask_migrate import Migrate
 from sys import exit
 from decouple import config
 
-from config import config_dict
-from app import create_app, db
+from apps.config import config_dict
+from apps import create_app, db
 
 # WARNING: Don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
