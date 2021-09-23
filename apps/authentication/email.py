0ca5d04e1885b2fbb5288d35799370a935444421
App Generator <support@appseed.us>
2021-09-23 12:25:38 +0300

    Email Sending - Update on handling errors

--- apps/authentication/email.py
+++ apps/authentication/email.py
@@ -6,13 +6,25 @@ Copyright (c) 2019 - present AppSeed.us
 from flask_mailman import EmailMessage
 from apps.config import Config
 
+from flask import current_app
+
 def send_email(subject, body, send_to):
 
-    msg = EmailMessage(
-        subject,
-        body,
-        Config.MAIL_USERNAME,
-        [send_to]
-    )
-    msg.content_subtype = "html"
-    msg.send()
+    try:
+
+        msg = EmailMessage(
+            subject,
+            body,
+            Config.MAIL_USERNAME,
+            [send_to]
+        )
+        msg.content_subtype = "html"
+        msg.send()
+
+        return True
+    
+    except:
+        current_app.logger.info('ERR - Cannot send email, please check MAIL settings')
+        current_app.logger.info('    -> MAIL Subject: ' + subject )
+        current_app.logger.info('    -> MAIL Body: ' + body )
+        return False
